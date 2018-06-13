06b250dde38d186f42507166cc101950c086153d
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-06-14 02:31:30 +0300

    feat: add eslint integration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './Header';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Header from "./Header";
+import App from "./App.css";
 
 class App extends Component {
   render() {
