036c6297ec02351e543707c27157bec105e557f9
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-06-15 02:08:45 +0300

    feat: add airbnb config

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import Profile from "./Profile/index";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Redirect from="/" to="/EveryInteract" />
+          <Route exact path="/EveryInteract" component={Profile} />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "normalize.css";
+import "flexboxgrid2/flexboxgrid2.css";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
