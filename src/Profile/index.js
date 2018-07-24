5a043221466d1fc96e9b491a8bb850c0204172b7
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-07-24 22:18:28 +0300

    chore: correct flow errors

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -30,27 +30,29 @@ type Props = {
 type State = {
   error: ?Object,
   isLoaded: boolean,
-  userInfo: {
-    id: string,
-    username: string,
-    acct: string,
-    display_name: string,
-    locked: boolean,
-    bot: boolean,
-    created_at: string,
-    note: string,
-    url: string,
-    avatar: string,
-    avatar_static: string,
-    header: string,
-    header_static: string,
-    followers_count: number,
-    following_count: number,
-    statuses_count: number,
-    emojis: (?Object)[],
-    fields: (?Object)[],
-    error?: string
-  }
+  userInfo:
+    | {
+        id: string,
+        username: string,
+        acct: string,
+        display_name: string,
+        locked: boolean,
+        bot: boolean,
+        created_at: string,
+        note: string,
+        url: string,
+        avatar: string,
+        avatar_static: string,
+        header: string,
+        header_static: string,
+        followers_count: number,
+        following_count: number,
+        statuses_count: number,
+        emojis: (?Object)[],
+        fields: (?Object)[],
+        error?: string
+      }
+    | Object
 };
 
 class Profile extends React.Component<Props, State> {
